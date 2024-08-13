7dfecd904e0aa59ad8212a6d3308fab46fb3f702
syyeong <sonssan0917@gmail.com>
2024-08-13 15:58:32 +0900

    Update and rename MyPage.styled.js to MyInfo.styled.js

--- frontend/src/pages/MyInfo/MyInfo.styled.js
+++ frontend/src/pages/MyInfo/MyInfo.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { FaBookmark } from 'react-icons/fa';
 
-export const MyPageContainer = styled.div`
+export const MyInfoContainer = styled.div`
   display: flex;
   height: 100vh;
 `;
