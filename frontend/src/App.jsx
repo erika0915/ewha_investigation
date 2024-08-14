715ab4b9cee2e5dc8e426fedc7ba161b999722ef
syyeong <sonssan0917@gmail.com>
2024-08-14 23:03:26 +0900

    chore(App.jsx): route에 switch 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
+import ProductList from './ProductList';
+import ProductManagement from './ProductManagement';
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import DepositPage from "./pages/ProductPage/DepositPage";
@@ -7,13 +10,17 @@ import LoanPage from "./pages/ProductPage/LoanPage";
 import CheckCardPage from "./pages/ProductPage/CheckCardPage";
 import { GlobalStyle } from "./pages/ProductPage/styles";
 import DetailedPage from "./pages/DetailedPage/DetailedPage";
-import MyPage from "./pages/MyPage/MyPage"; // MyPage 컴포넌트
+import MyPage from "./pages/MyPage/MyPage"; 
 import Main from "./pages/Main/Main";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Switch>
+          <Route path="/" exact component={ProductList} /> 
+          <Route path="/product-management" component={ProductManagement} />
+        </Switch>
         <Route path="/" element={<Main />} />{" "}
         {/* 기본 경로를 MyPage로 리다이렉트 */}
         <Route path="/mypage" element={<MyPage />} /> {/* MyPage 페이지 */}
@@ -31,5 +38,8 @@ const App = () => {
     </Router>
   );
 };
+
+export default App;
+
 
 export default App;
