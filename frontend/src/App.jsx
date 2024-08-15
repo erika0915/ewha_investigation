20f9bcc3254d35452edfc64034f7f1fe9221b7fd
syyeong <sonssan0917@gmail.com>
2024-08-15 19:05:03 +0900

    refactore(Admin): 라우트를 Link로 변환, 스위치 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import ProductList from './ProductList';
 import ProductManagement from './ProductManagement';
 import Login from "./pages/Login/Login";
@@ -16,28 +16,37 @@ import Main from "./pages/Main/Main";
 const App = () => {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Main</Link></li>
+          <li><Link to="/product-management">Product Management</Link></li>
+          <li><Link to="/mypage">MyPage</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/deposit">Deposit</Link></li>
+          <li><Link to="/saving">Saving</Link></li>
+          <li><Link to="/loan">Loan</Link></li>
+          <li><Link to="/checkcard">Check Card</Link></li>
+        </ul>
+      </nav>
       <Routes>
-        <Switch>
-          <Route path="/" exact component={ProductList} /> 
-          <Route path="/product-management" component={ProductManagement} />
-        </Switch>
-        <Route path="/" element={<Main />} />{" "}
-        {/* 기본 경로를 MyPage로 리다이렉트 */}
-        <Route path="/mypage" element={<MyPage />} /> {/* MyPage 페이지 */}
-        <Route path="/main" element={<Main />} /> {/* Main 페이지 */}
-        <Route path="/login" element={<Login />} /> {/* Login 페이지 */}
-        <Route path="/signup" element={<SignUp />} /> {/* SignUp 페이지 */}
-        <Route path="/deposit" element={<DepositPage />} />{" "}
-        {/* DepositPage 페이지 */}
-        <Route path="/saving" element={<SavingPage />} />{" "}
-        {/* SavingPage 페이지 */}
-        <Route path="/loan" element={<LoanPage />} /> {/* LoanPage 페이지 */}
-        <Route path="/checkcard" element={<CheckCardPage />} />{" "}
-        {/* CheckCardPage 페이지 */}
+        <Route path="/" element={<Main />} /> 
+        <Route path="/product-management" element={<ProductManagement />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/deposit" element={<DepositPage />} />
+        <Route path="/saving" element={<SavingPage />} />
+        <Route path="/loan" element={<LoanPage />} />
+        <Route path="/checkcard" element={<CheckCardPage />} />
+        <Route path="/detailedpage" element={<DetailedPage />} />
       </Routes>
     </Router>
   );
 };
+
+export default App;
+
 
 export default App;
 
