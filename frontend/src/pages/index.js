7ad3f948a32abb1c5a3ef34bb860e3cf5e6e0b09
minnje0ng <laputais34@gmail.com>
2024-08-17 12:47:51 +0900

    feat(banksearch): 은행 검색 페이지 생성

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,4 +2,5 @@ export { Login } from "./Login";
 export { SignUp } from "./SignUp";
 export { Main } from "./Main";
 export { DetailedPage } from "./DetailedPage";
+export { default as BankPage } from "./BankPage";
 export * from "./ProductPage";
